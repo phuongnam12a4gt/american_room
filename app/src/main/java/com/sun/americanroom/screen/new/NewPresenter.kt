1437efd5043f86e95fb31680206cbcee573adf99
hoangphuc <vodanh1134dn@gmail.com>
2021-03-29 15:12:27 +0700

    Fix convention

---
+++ app/src/main/java/com/sun/americanroom/screen/new/NewPresenter.kt
@@ -0,0 +1,65 @@
+package com.sun.americanroom.screen.new
+
+import com.sun.americanroom.data.model.City
+import com.sun.americanroom.data.source.remote.OnFetchDataJsonListener
+import com.sun.americanroom.data.source.repository.RoomRepository
+import com.sun.americanroom.screen.top.TopRoomFragment
+import com.sun.americanroom.utils.StateCode
+import com.sun.americanroom.utils.StateName
+import java.lang.Exception
+
+class NewPresenter (
+    private val repository: RoomRepository
+) : NewContract.Presenter {
+
+    private var view: NewContract.View? = null
+
+    override fun getCity(state: String, numberOfCity: Int): MutableList<City> {
+        var listCity = mutableListOf<City>()
+        repository.getCity(object : OnFetchDataJsonListener<MutableList<City>> {
+            override fun onSuccess(data: MutableList<City>) {
+                view?.getCityOnSuccess(data)
+                listCity = data
+            }
+
+            override fun onError(exception: Exception?) {
+                view?.onError(exception)
+            }
+        }, state, numberOfCity)
+        return listCity
+    }
+
+    override fun getCityFromPick(state: String): MutableList<City>? =
+        when (state) {
+            StateName.CALIFORNIA -> {
+                getCity(StateCode.CALIFORNIA, NUMBER_OF_CITY)
+            }
+            StateName.GEORGIA -> {
+                getCity(StateCode.GEORGIA, NUMBER_OF_CITY)
+            }
+            StateName.MICHIGAN -> {
+                getCity(StateCode.MICHIGAN, NUMBER_OF_CITY)
+
+            }
+            StateName.WASHINGTON -> {
+                getCity(StateCode.WASHINGTON, NUMBER_OF_CITY)
+
+            }
+            StateName.FLORIDA -> {
+                getCity(StateCode.FLORIDA, NUMBER_OF_CITY)
+            }
+            else -> null
+        }
+
+    override fun onStart() {}
+
+    override fun onStop() {}
+
+    override fun setView(view: NewContract.View?) {
+        this.view = view
+    }
+
+    companion object {
+        private const val NUMBER_OF_CITY = 5
+    }
+}
---
+++ app/src/main/java/com/sun/americanroom/screen/new/roomitem/adapterroom/NewRoomAdapter.kt
@@ -0,0 +1,34 @@
+package com.sun.americanroom.screen.new.roomitem.adapterroom
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sun.americanroom.R
+import com.sun.americanroom.data.model.NewRoom
+
+class NewRoomAdapter : RecyclerView.Adapter<ItemNewRoomViewHolder>() {
+
+    private val newRoomList = mutableListOf<NewRoom>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ): ItemNewRoomViewHolder =
+        ItemNewRoomViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_new_room, parent, false
+            )
+        )
+
+    override fun getItemCount() = newRoomList.size
+
+    override fun onBindViewHolder(holder: ItemNewRoomViewHolder, position: Int) =
+        holder.bindViewData(newRoomList[position])
+
+    fun addData(newRooms: MutableList<NewRoom>) {
+        newRooms?.let {
+            newRoomList.clear()
+            newRoomList.addAll(it)
+            notifyDataSetChanged()
+        }
+    }
+}
